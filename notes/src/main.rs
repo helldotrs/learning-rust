4b5be97fc26de2c6fdc552c619dc4b39942096a9
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-04 22:24:23 +0200

    adding clone

--- notes/src/main.rs
+++ notes/src/main.rs
@@ -2,5 +2,12 @@ fn main() {
     println!("Hello, world!");
 
     let x = 2;
+    println!("{}", x);
+
     let y = x; //changes owner of value to x, and drops y variable. y variable can no longer be refered it.
+    //println!("{}", x); // would not compile.
+
+    let z = y.clone(); // creates new value for z, y can still be used. 
+    //(a clone might in other languages be refered to as "a deep copy")
+
 }
