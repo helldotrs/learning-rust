0713d28efc24b97cf2f3cca2e6a0dee22372f0f7
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-04 22:34:20 +0200

    adding add_one fn

--- notes/src/main.rs
+++ notes/src/main.rs
@@ -4,10 +4,22 @@ fn main() {
     let x = 2;
     println!("{}", x);
 
-    let y = x; //changes owner of value to x, and drops y variable. y variable can no longer be refered it.
+    let mut y = x; //changes owner of value to x, and drops y variable. y variable can no longer be refered it.
     //println!("{}", x); // would not compile.
 
     let z = y.clone(); // creates new value for z, y can still be used. 
     //(a clone might in other languages be refered to as "a deep copy")
 
+    y = add_one(y); //once var is sent to func var is dropped: unusuall but valid solution, normally you'd use a value.
+    // y == 3;
+
+    
+
+    
 }
+
+fn add_one (a: String) -> String{
+    a + 1
+}
+
+
