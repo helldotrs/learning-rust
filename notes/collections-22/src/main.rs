a35b6a268c38f59fc480baf0c3dbbbb905c3c44e
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-10 21:16:03 +0200

    Update main.rs

--- notes/collections-22/src/main.rs
+++ notes/collections-22/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("Hello, world!");
     let mut my_first_vector: Vec<i32> = Vec::new(); 
+    // can also be achived with
+    let mut my_second_vector = vec![2, 4, 6, 8];
 
     my_first_vector.push(2);
     my_first_vector.push(4);
@@ -11,6 +13,8 @@ fn main() {
     my_first_vector.pop();
     println!("vector length is {}", my_first_vector.len());
     println!("the first value stored is {}", my_first_vector[0])
+
+    //let mut my_first_hash_map
 }
 
 
