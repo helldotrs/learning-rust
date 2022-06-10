96c544c489b01f1b9da13122b3f0b32b2dd1187d
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-10 21:13:22 +0200

    Update main.rs

--- notes/collections-22/src/main.rs
+++ notes/collections-22/src/main.rs
@@ -5,8 +5,12 @@ fn main() {
     my_first_vector.push(2);
     my_first_vector.push(4);
     my_first_vector.push(6);
-
+    my_first_vector.push(8);
     println!("vector length is {}", my_first_vector.len());
+
+    my_first_vector.pop();
+    println!("vector length is {}", my_first_vector.len());
+    println!("the first value stored is {}", my_first_vector[0])
 }
 
 
