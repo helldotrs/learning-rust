568f1d2cdaa9a695782d4926d27ac61e8055b850
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-10 21:49:25 +0200

    Update main.rs

--- notes/enums-23/src/main.rs
+++ notes/enums-23/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     println!("Hello, world!");
     //more like Algebraic Data  Types in Haskall than C-like enums
+    //"like a union in C only so much better"
 
     enum Color {
         Red,
         Green,
-        Blue
+        Blue,
+    }
+
+    //let color = Color::Red();
+
+    enum DispenserItem {
+        Empty,
+        Ammo(u8),
+        Things(String, i32),
+        Place {x: i32, y: i32},
     }
 }
