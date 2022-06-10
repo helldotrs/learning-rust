f77b96ec7b09b47071533cf81c3c37dc0c5664ae
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-10 21:43:09 +0200

    Create main.rs

---
+++ notes/enums-23/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    //more like Algebraic Data  Types in Haskall than C-like enums
+
+    enum Color {
+        Red,
+        Green,
+        Blue
+    }
+}
