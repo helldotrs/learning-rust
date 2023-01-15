58e1a4aa2e673ef65b4ab0f5b8f1f9392cca68a1
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-15 19:16:47 +0100

    Update main.rs

--- dcode/hello-world/src/main.rs
+++ dcode/hello-world/src/main.rs
@@ -121,7 +121,7 @@ fn main() {
     println!("{}", (tup1.7).0);
     println!("{}", mac);
 
-    print_number_to(5);
+    print_even_number_to(20);
 
     if is_even(2){
         println!("even");
@@ -151,9 +151,13 @@ fn main() {
 
 }
 
-fn print_number_to(num: u32){
+fn print_even_number_to(num: u32){
     for n in 1..num { //non-inclusive 
-        println!("{}", n);
+        if is_even(n) {
+            println!("{} is even", n);
+        } else {
+            println!("{} is odd", n);
+        }
     }
 }
 
