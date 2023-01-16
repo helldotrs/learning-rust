03f0c423bcc2343921768a78ec41b6666ea5caa1
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-16 22:29:17 +0100

    Update main.rs

--- dcode/structs-17/src/main.rs
+++ dcode/structs-17/src/main.rs
@@ -1,15 +1,27 @@
-struct Color {
+struct Color { //struct
     red: u8, // u8  -> 0-255
     green: u8,
     blue: u8
 }
 
+struct Tsc ( //touple struct //TSC -ToupleStructColor
+    u8, u8, u8
+);
+
 fn main(){
     let mut bg = Color { red: 225, green: 70, blue: 15};
 
-    println!("bg: {}, {}, {}", bg.red, bg.green, bg.blue)
+    println!("bg: {}, {}, {}", bg.red, bg.green, bg.blue);
 
     bg.blue = 45;
 
-    println!("bg: {}, {}, {}", bg.red, bg.green, bg.blue)
+    println!("bg: {}, {}, {}", bg.red, bg.green, bg.blue);
+
+    let mut red = Tsc(255, 0 ,0);
+
+    red.2 = 45;
+
+    println!("red: {}, {}, {}", red.0, red.1, red.2);
+
+
 }
