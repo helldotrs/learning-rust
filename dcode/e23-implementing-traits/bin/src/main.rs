1b127a89d27426e9a8d828b1aa7195d922bee533
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-25 21:20:04 +0100

    Update main.rs

--- dcode/e23-implementing-traits/bin/src/main.rs
+++ dcode/e23-implementing-traits/bin/src/main.rs
@@ -3,6 +3,14 @@ struct Person {
     age: u8
 }
 
+impl ToString for Person{
+    fn to_string(&self) -> String {
+        format!("My name is {} and I am {}.", self.name, self.age)
+    }
+}
+
 fn main() {
     let bekah = Person { name: String::from("Rebekah"), age: 123};
+
+    println!("{}", bekah.to_string());
 }
