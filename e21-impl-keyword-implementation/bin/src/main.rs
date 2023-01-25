8c272401b071cd96415496800fd5291bf1ccd040
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-25 07:04:23 +0100

    Create main.rs

---
+++ e21-impl-keyword-implementation/bin/src/main.rs
@@ -0,0 +1,16 @@
+struct Rectangle {
+    width:  u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn print_description(&self){
+        println!("Rectangle: {} x {}", self.width, self.height)
+    }
+}
+
+fn main() {
+    let my_rect = Rectangle{width: 10, height: 5};
+
+    my_rect.print_description();
+}
