d9613c1437084d1681f032bed6c1d987317aa8ef
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-27 17:03:20 +0100

    Update main.rs

--- dcode/e29-pattern-matching/bin/src/main.rs
+++ dcode/e29-pattern-matching/bin/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
         1           => println!("The one!"),
         2..=9       => println!("TWO-nine!!"), // ..= for inclusive range.   
                                                // ... gives: rustc --explain E0658
+                                               // unstable feature, only works in nightly build, not in beta/stable
         10 | 11     => println!("ten or eleven"), //can not use or/OR apparently :(
         _           => println!("no match!") //_ for "else"
     }
