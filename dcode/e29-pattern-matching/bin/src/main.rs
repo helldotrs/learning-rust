09f906558a453157078aefba674e55c638e0af2b
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-27 16:14:37 +0100

    Update main.rs

--- dcode/e29-pattern-matching/bin/src/main.rs
+++ dcode/e29-pattern-matching/bin/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    let number = 2;
+
+    match number{ //"switch"
+        1 => println!("The one!"),
+        2 => println!("TWO!!"),
+        _ => println!("no match!") //_ for "else"
+    }
     println!("Hello, world!");
 }
