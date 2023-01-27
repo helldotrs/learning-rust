83f9a44c0d032eb98515a907332687f786d49b28
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-27 17:01:07 +0100

    Update main.rs

--- dcode/e29-pattern-matching/bin/src/main.rs
+++ dcode/e29-pattern-matching/bin/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    let number = 11;
+    let number = 9;
 
     match number{ //"switch"
         1           => println!("The one!"),
-        2           => println!("TWO!!"),
+        2..=9       => println!("TWO-nine!!"), // ..= for inclusive range.   
+                                               // ... gives: rustc --explain E0658
         10 | 11     => println!("ten or eleven"), //can not use or/OR apparently :(
         _           => println!("no match!") //_ for "else"
     }
