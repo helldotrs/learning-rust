0daa6c92aafc0b38c1dc2ab2fef1bcb1a48a1af8
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-27 16:55:06 +0100

    Update main.rs

--- dcode/e29-pattern-matching/bin/src/main.rs
+++ dcode/e29-pattern-matching/bin/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    let number = 2;
+    let number = 11;
 
     match number{ //"switch"
-        1 => println!("The one!"),
-        2 => println!("TWO!!"),
-        _ => println!("no match!") //_ for "else"
+        1           => println!("The one!"),
+        2           => println!("TWO!!"),
+        10 | 11     => println!("ten or eleven"), //can not use or/OR apparently :(
+        _           => println!("no match!") //_ for "else"
     }
     println!("Hello, world!");
 }
