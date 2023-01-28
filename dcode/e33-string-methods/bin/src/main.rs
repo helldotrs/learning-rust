d4041ed783b257838b00d192c01eb3110a18960b
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-28 17:51:53 +0100

    Create main.rs

---
+++ dcode/e33-string-methods/bin/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    {
+        let my_string = String::from("Rust is the awesome!");
+        println!("after replace: {}", my_string.replace("awesome", "the best"));
+    }
+    println!("Hello, world!");
+}
