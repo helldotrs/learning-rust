6e71bbcc02b3e7ac405a43180f49367d8aff6b22
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-28 06:19:33 +0100

    Update main.rs

--- dcode/e32-random-numbers-and-booleans/bin/src/main.rs
+++ dcode/e32-random-numbers-and-booleans/bin/src/main.rs
@@ -2,6 +2,6 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1,11); //1-10
+    let random_number = rand::thread_rng().gen_range(1..11); //1-10
     println!("random number: {}", random_number);
 }
