a262ef9e544e640512bdb2a9df0421bd769c7aae
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-28 06:28:17 +0100

    Update main.rs

--- dcode/e32-random-numbers-and-booleans/bin/src/main.rs
+++ dcode/e32-random-numbers-and-booleans/bin/src/main.rs
@@ -4,4 +4,7 @@ use rand::Rng;
 fn main() {
     let random_number = rand::thread_rng().gen_range(1..11); //1-10
     println!("random number: {}", random_number);
+
+    let random_boolean = rand::thread_rng().gen_bool(1.0/2.0); //passing 2 means it has 1 in 2 chance of turning out true.
+    println!("coin toss: {}", random_boolean)
 }
