9af7ee1abdb3523bb72bfd5f7a81483859e795a3
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-28 18:15:40 +0100

    Update main.rs

--- dcode/e33-string-methods/bin/src/main.rs
+++ dcode/e33-string-methods/bin/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
 
     {
-        let my_string = String::from("Rust is the awesome!");
+        let my_string = String::from("Rust is awesome!");
         println!("after replace: {}", my_string.replace("awesome", "the best"));
     }
     println!("Hello, world!");
+
+    {
+        let my_string = String::from("Rust\n is\n awesome!");
+        println!("{}",my_string);
+        
+        for line in my_string.lines(){
+            print!("[ {} ]", line);
+        }
+    }
 }
