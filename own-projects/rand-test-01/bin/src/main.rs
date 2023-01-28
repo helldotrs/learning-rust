bd32e0fd5f0055ded8acf2ae788981f4500b9c04
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-28 06:39:46 +0100

    Create main.rs

---
+++ own-projects/rand-test-01/bin/src/main.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+use rand::Rng;
+
+fn main() {
+
+    let mut num1    = 0;
+    let mut num2    = 0;
+    let mut num3    = 0;
+
+    let mut i       = 0;
+    while i < 1000000 {
+        match rand::thread_rng().gen_range(1..4){
+            1 => num1 = num1 + 1,
+            2 => num2 = num2 + 1,
+            3 => num3 = num3 + 1,
+            _ => break
+        }
+
+        i = i + 1;
+    }
+    println!("{}, {}, {}", num1, num2, num3);
+}
