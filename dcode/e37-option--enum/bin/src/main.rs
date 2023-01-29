d42a76776f7d6e3607033511c32dcb838ab9c207
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-01-29 05:12:08 +0100

    Update main.rs

--- dcode/e37-option--enum/bin/src/main.rs
+++ dcode/e37-option--enum/bin/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     
     let name        = String::from("Hellmak");
-    let name_long   = String::from("HellmakHellmak");
 
     
     println!("Char at index 8: {}", match name.chars().nth(8){ //chars return iterator with chars, nth get item at passed index
@@ -9,7 +8,7 @@ fn main() {
         None    => "no character".to_string()
     });
     
-    println!("Char at index 8: {}", match name_long.chars().nth(8){ //chars return iterator with chars, nth get item at passed index
+    println!("Char at index 8: {}", match name.chars().nth(0){ //chars return iterator with chars, nth get item at passed index
         Some(a) => a.to_string(),
         None    => "no character".to_string()
     });
