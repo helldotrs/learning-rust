3f517a74740c03f28cd1d60967926c94b791779a
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-01 14:57:08 +0100

    Update main.rs

--- dcode/e40-running-executing-commands--CLI/bin/src/main.rs
+++ dcode/e40-running-executing-commands--CLI/bin/src/main.rs
@@ -1,3 +1,22 @@
-fn main {
-  println!("hello kitty!");
+use std::process::Command;
+
+
+fn main() {
+    let mut my_cmd = Command::new("python");
+    my_cmd.arg("script.py");
+
+    match my_cmd.output() {
+        Ok(a)   => {
+            unsafe{
+                println!("output: {}", String::from_utf8_unchecked(a.stdout));
+            }
+        }
+
+        Err(a)  => {
+            println!("error: {}", a);
+        }
+
+    }
+
+    println!("Hello, world!");
 }
