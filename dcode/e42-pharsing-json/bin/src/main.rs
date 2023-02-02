efde9171881b3bfbd2624ef1132d7c5b49f4c00b
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-02 10:06:44 +0100

    Update main.rs

--- dcode/e42-pharsing-json/bin/src/main.rs
+++ dcode/e42-pharsing-json/bin/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+extern crate serde_json;
 
+use serde_json::Value as JsonValue;
+
+fn main() {
+    let json_str = r#"
+    
+    "#; // Rust raw-string
+    println!("Hello, world!");
 }
