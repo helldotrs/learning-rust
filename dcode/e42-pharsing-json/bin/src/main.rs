9170a14a3923b16e5d48d192f87bfe79600b5d7c
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-02 14:38:17 +0100

    Update main.rs

--- dcode/e42-pharsing-json/bin/src/main.rs
+++ dcode/e42-pharsing-json/bin/src/main.rs
@@ -1,6 +1,16 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
 
 use serde_json::Value as JsonValue;
+
+#[derive(Serialize, Deserialize)]
+struct Person{
+    name:   String,
+    age:    u8,
+    is_male: bool
+}
 
 fn main() {
     let json_str = r#"
@@ -14,8 +24,10 @@ fn main() {
     let res = serde_json::from_str(json_str);
 
     if res.is_ok(){
-        let p: JsonValue = res.unwrap();
-        println!("The name is {}", p["name"]);
+        /*let p: JsonValue = res.unwrap();
+        println!("The name is {}", p["name"]);*/
+        let p: Person = res.unwrap();
+        println!("{}", p.name)
     } else {
         println!("could not parse JSON");
     }
