8f0c25c2d9d115fa82046b9efd5210d57d99ee45
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-05 08:52:36 +0100

    Create main.rs

---
+++ doc.rust-lang.org/book/ch02-00-guessing-game-tutorial-html/src/main.rs
@@ -0,0 +1,21 @@
+use std::io; // io --> input/output
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess String::new();
+    /*
+        The :: syntax in the ::new line indicates that new is an associated function of the String type. An associated function is a function that’s implemented on a type, in this case String. This new function creates a new, empty string. You’ll find a new function on many types because it’s a common name for a function that makes a new value of some kind.
+    */
+
+    io::stdin()
+        .read_line(mut &guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+
+
+}
