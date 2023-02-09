71efbeef71cb73c329513753129dba7471d40c33
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-09 15:15:37 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch02-00-guessing-game-tutorial-html/src/main.rs
+++ doc.rust-lang.org/book/ch02-00-guessing-game-tutorial-html/src/main.rs
@@ -12,29 +12,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
- 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    /*
-        The :: syntax in the ::new line indicates that new is an associated function of the String type. An associated function is a function that’s implemented on a type, in this case String. This new function creates a new, empty string. You’ll find a new function on many types because it’s a common name for a function that makes a new value of some kind.
-    */
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
     
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    //u32 --> small positive number.
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+        /*
+            The :: syntax in the ::new line indicates that new is an associated function of the String type. An associated function is a function that’s implemented on a type, in this case String. This new function creates a new, empty string. You’ll find a new function on many types because it’s a common name for a function that makes a new value of some kind.
+        */
 
-    match guess.cmp(&secret_number){ //The cmp method compares two values and can be called on anything that can be compared. 
-        Ordering::Less      => println!("Too small!"),
-        Ordering::Greater   => println!("Too big!"),
-        Ordering::Equal     => println!("U r winrar!")
-    } //the Ordering type is another enum and has the variants Less, Greater, and Equal
-    //The match expression ends after the first successful match
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        //u32 --> small positive number.
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number){ //The cmp method compares two values and can be called on anything that can be compared. 
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => println!("U r winrar!")
+        } //the Ordering type is another enum and has the variants Less, Greater, and Equal
+        //The match expression ends after the first successful match
+    }
 
 
 }
