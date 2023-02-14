472dae8ecf7799141b583883e3217a1d7ed1e3c8
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-14 15:43:58 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-02-data-types.html/tuple/bin/src/main.rs
+++ doc.rust-lang.org/book/ch03-02-data-types.html/tuple/bin/src/main.rs
@@ -6,4 +6,7 @@ fn main() {
     println!("The value of y is: {y}");
 
     println!("first element is {}", tup.0);
+    
+    //rust tuple: fixed length, can contain different data types
+    //rust array: fixed length, must contain same data type
 }
