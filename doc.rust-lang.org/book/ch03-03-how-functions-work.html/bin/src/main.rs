15b75df267ac71749a9705cd7943af26b227cd0b
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-15 17:16:08 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
+++ doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
@@ -1,8 +1,13 @@
 fn main(){
   another_function(5); //snake_case for functions in rust as per convention.
+  
+  print_labeled_measurement(5, 'h'); //single qoute for char
 }
 
 fn another_function(x: i31){  //must declare value type for each parameter in a function signature
-  println!("value of x is {}", x);
+  println!("Value of x is {}", x);
 }
 
+fn print_labeled_measurement(value: i32, unit_label: char){
+  println!("Measurement: {value}{unit_label}"); // is there any convention for when to use "{x}" vs "{}", x??
+}
