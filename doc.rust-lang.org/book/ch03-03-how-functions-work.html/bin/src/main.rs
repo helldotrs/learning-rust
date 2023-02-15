77feeff5e4a9e0abc0ad5000271979ff1029f955
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-15 17:11:04 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
+++ doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
@@ -2,7 +2,7 @@ fn main(){
   another_function(5); //snake_case for functions in rust as per convention.
 }
 
-fn another_function(x: i31){
+fn another_function(x: i31){  //must declare value type for each parameter in a function signature
   println!("value of x is {}", x);
 }
 
