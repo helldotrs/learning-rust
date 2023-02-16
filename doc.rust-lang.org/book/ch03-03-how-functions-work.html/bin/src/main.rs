f6092137e555701db234f975d32c30daa1c68ac1
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-16 11:01:13 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
+++ doc.rust-lang.org/book/ch03-03-how-functions-work.html/bin/src/main.rs
@@ -2,6 +2,13 @@ fn main(){
   another_function(5); //snake_case for functions in rust as per convention.
   
   print_labeled_measurement(5, 'h'); //single qoute for char
+
+  fn scope_test(){
+    println!("yay! :D");
+  }
+  scope_test();
+ 
+  
 }
 
 fn another_function(x: i31){  //must declare value type for each parameter in a function signature
