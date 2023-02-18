400a7885ef391bb27a97ff6a62343dcddade1e65
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-18 12:06:13 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-05-control-flow.html/src/main.rs
+++ doc.rust-lang.org/book/ch03-05-control-flow.html/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     //Because if is an expression, we can use it on the right side of a let statement to assign the outcome to a variable:
     let condition = true;
     let number = if condition { 5 } else { 6 };
-
     println!("The value of number is: {number}");
+    
+    //the return value has to be the same type, hence this wont compile:
+    //let number = if condition { 5 } else { "six" };
 }
