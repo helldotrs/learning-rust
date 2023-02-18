0ef637e3f2a2de540b7308db7834d01f9149ba02
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-18 12:03:30 +0100

    Create main.rs

---
+++ doc.rust-lang.org/book/ch03-05-control-flow.html/src/main.rs
@@ -0,0 +1,9 @@
+
+
+fn main() {
+    //Because if is an expression, we can use it on the right side of a let statement to assign the outcome to a variable:
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
