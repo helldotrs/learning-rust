5371771493bce15c15af7958c5fadca41643dca9
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-19 12:20:56 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch03-05-control-flow.html/src/main.rs
+++ doc.rust-lang.org/book/ch03-05-control-flow.html/src/main.rs
@@ -20,9 +20,25 @@ fn main() {
     };
     println!("The result is {result}");
     
-    // infinity loop:
-    loop {
-        println("ctrl-c to exit");
+    //loop label
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
     }
+    println!("End count = {count}");
 }
 }
