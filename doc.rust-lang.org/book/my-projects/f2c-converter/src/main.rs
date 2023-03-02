f15941b87a248261906d0c43101488d0ce74fef6
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-02 18:44:13 +0100

    Create main.rs

---
+++ doc.rust-lang.org/book/my-projects/f2c-converter/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut input: f64 = 100.0;
+    println!("Hello, world!");
+
+    fn f2c --> f64 (a){  // FIXME: ref
+        a = a - 32;
+        a = a * 5;
+        a = a / 9;
+        a
+    }
+
+}
