aff6ffe32329a042c06931c757a6ff0845b701b7
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-05 22:46:12 +0100

    Update main.rs

--- chat-gpt/celsius-fahrenheit--fn-and-borrowing/src/main.rs
+++ chat-gpt/celsius-fahrenheit--fn-and-borrowing/src/main.rs
@@ -1,21 +1,24 @@
-/* Code from ChatGPT, own comments will be in /// or  /** **/
-*/
-
-/*
-Writte me the source code for a celsius fahrenheit converter
-that works both ways written in Rust that is well commented and
-that uses functions and borrowing. it is to help me udnerstand Rust concepts*/
-
 use std::io;
 
-// This function takes a temperature in Celsius as input and returns the corresponding temperature in Fahrenheit
+// This function takes a reference to a temperature in Celsius as input
+// and returns the corresponding temperature in Fahrenheit
 fn celsius_to_fahrenheit(celsius: &f32) -> f32 {
+    // Here, we are borrowing the value of celsius, rather than taking
+    // ownership of it. This means that the calling code retains ownership
+    // of the value, and we can only read its value without modifying it.
+    // This function returns a new value, which does not affect the original
+    // value of celsius.
     celsius * 1.8 + 32.0
-    /// borrows the celcious value, and returns a value it has ownership of?
 }
 
-// This function takes a temperature in Fahrenheit as input and returns the corresponding temperature in Celsius
+// This function takes a reference to a temperature in Fahrenheit as input
+// and returns the corresponding temperature in Celsius
 fn fahrenheit_to_celsius(fahrenheit: &f32) -> f32 {
+    // Here, we are again borrowing the value of fahrenheit, rather than
+    // taking ownership of it. This means that the calling code retains
+    // ownership of the value, and we can only read its value without
+    // modifying it. This function returns a new value, which does not affect
+    // the original value of fahrenheit.
     (fahrenheit - 32.0) / 1.8
 }
 
@@ -53,6 +56,12 @@ fn main() {
 
             // Call the celsius_to_fahrenheit function to convert the temperature
             let fahrenheit = celsius_to_fahrenheit(&celsius);
+            // Here, we are passing a reference to the value of celsius to
+            // the celsius_to_fahrenheit function. This means that we are
+            // borrowing the value of celsius and passing it to the function
+            // for the duration of the function call. The function can read
+            // the value of celsius, but it cannot modify it, and it cannot
+            // outlive the function call.
 
             // Print the result
             println!("{}°C is equivalent to {}°F", celsius, fahrenheit);
@@ -66,26 +75,5 @@ fn main() {
             io::stdin().read_line(&mut fahrenheit_input).expect("Failed to read line");
 
             // Parse the user's input as a float
-            let fahrenheit: f32 = match fahrenheit_input.trim().parse() {
-                Ok(num) => num,
-                Err(_) => panic!("Invalid input"),
-            };
+           
 
-            // Call the fahrenheit_to_celsius function to convert the temperature
-            let celsius = fahrenheit_to_celsius(&fahrenheit);
-
-            // Print the result
-            println!("{}°F is equivalent to {}°C", fahrenheit, celsius);
-        }
-        _ => {
-            println!("Invalid choice. Please enter 1 or 2.");
-        }
-    }
-}
-
-/*
-In this implementation, we define two functions: celsius_to_fahrenheit and fahrenheit_to_celsius. Both functions take a reference to a float as input (i.e., they borrow the value rather than taking ownership of it), and they return a float. These functions use the formulas for converting between Celsius and Fahrenheit temperatures.
-
-In the main function, we prompt the user to choose which conversion they want to perform. We read the user's choice, parse it as an integer, and use a match expression to perform the corresponding conversion. In each case, we prompt the user to enter a temperature, read their input
-
-*/
