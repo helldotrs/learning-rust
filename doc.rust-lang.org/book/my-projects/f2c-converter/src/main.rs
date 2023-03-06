e3d4e6e8c3d7600f812b7b20a7f93ddf4c22f839
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-06 16:56:12 +0100

    Update main.rs

--- doc.rust-lang.org/book/my-projects/f2c-converter/src/main.rs
+++ doc.rust-lang.org/book/my-projects/f2c-converter/src/main.rs
@@ -1,5 +1,14 @@
+use std::io;
+
+//rust docs declares functions after main and chat-gpt before. The advantage with declaring them before is that it feels a bit more logical, on the other hand; the advantage of writing them after is that it makes them a bit more abstract and may help when starting with psudocode. 
+//I think I will functions after main for now.
+
 fn main() {
-    let mut input: f64 = 100.0;
+    
+    println!("please enter value to convert (c-->f & f-->c)");
+    let mut input: string = String::new();
+    io::stdin().read_line(&mut input).expect("failure to read input");
+    
     println!("Hello, world!");
 
     fn f2c --> f64 (a){  // FIXME: ref
