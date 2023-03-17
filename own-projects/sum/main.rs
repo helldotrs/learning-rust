1cd18e3843d362e1d216fb9e7042fec226d46fc8
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-17 21:24:50 +0100

    Update main.rs

--- own-projects/sum/main.rs
+++ own-projects/sum/main.rs
@@ -1,6 +1,15 @@
 use std::io;
 
 fn main() {
+  println!("input a number");
+  
+  let mut num1 = String::new();
+  io::stdin().read_line(&mut num1).expect("failed to read line";
+    
+  let num1: f32 = num.trim().parse().expect("please enter valid number"); //should make into a function
+    //f32 includes negative, u32 does not
+    
+    
 
 
 }
