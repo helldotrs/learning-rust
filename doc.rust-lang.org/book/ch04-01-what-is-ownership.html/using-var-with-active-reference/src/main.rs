a49e7db2efbb4e5adfd92c68260bc064e70dfc7a
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-22 15:47:23 +0100

    Update main.rs

--- doc.rust-lang.org/book/ch04-01-what-is-ownership.html/using-var-with-active-reference/src/main.rs
+++ doc.rust-lang.org/book/ch04-01-what-is-ownership.html/using-var-with-active-reference/src/main.rs
@@ -1,4 +1,3 @@
-  GNU nano 5.4                                                  src/main.rs *                                                          
 fn main() {
         let sv = String::from("Rust!");
         println!("{}", sv);
@@ -7,9 +6,13 @@ fn main() {
                 println!("{}", sr); //should compile
                 println!("{}", sv); //compiles? it does, conslussion: I can call a String var while a reference is still in scope?
                                         //or is the reference dropped once it is not used any more? lets test.
-                println!("{}", sr); //still compiles!
-
+                println!("{}", sr); //still compiles!!
+                //what if we try to change the value of sv
+                let sv = String::from("C++");
+                println!("sv: {}, sr: {}", sv, sr); //does not change the value of sr
         }
 }
+
+
 
 
